428fd447f6086c280eee0ed9d2b5bade0e5ff454
Scott Marlow <smarlow@redhat.com>
2012-10-11 13:06:25 -0400

    add more debugging

--- core/src/main/java/org/infinispan/marshall/exts/ReplicableCommandExternalizer.java
+++ core/src/main/java/org/infinispan/marshall/exts/ReplicableCommandExternalizer.java
@@ -42,6 +42,8 @@ import org.infinispan.io.UnsignedNumeric;
 import org.infinispan.marshall.AbstractExternalizer;
 import org.infinispan.marshall.Ids;
 import org.infinispan.util.Util;
+import org.infinispan.util.logging.Log;
+import org.infinispan.util.logging.LogFactory;
 
 import java.io.IOException;
 import java.io.ObjectInput;
@@ -58,31 +60,44 @@ import java.util.Set;
 public class ReplicableCommandExternalizer extends AbstractExternalizer<ReplicableCommand> {
    private final RemoteCommandsFactory cmdFactory;
    private final GlobalComponentRegistry globalComponentRegistry;
-   
-   public ReplicableCommandExternalizer(RemoteCommandsFactory cmdFactory, GlobalComponentRegistry globalComponentRegistry) {
+   private static final Log log = LogFactory.getLog(ReplicableCommandExternalizer.class);
+
+    public ReplicableCommandExternalizer(RemoteCommandsFactory cmdFactory, GlobalComponentRegistry globalComponentRegistry) {
       this.cmdFactory = cmdFactory;
       this.globalComponentRegistry = globalComponentRegistry;
    }
 
    @Override
    public void writeObject(ObjectOutput output, ReplicableCommand command) throws IOException {
+      log.infof("ReplicableCommandExternalizer.writeObject() 1. start writing new object for commandId=%d, parameter count=%d",
+              command.getCommandId(),
+              command.getParameters() != null? command.getParameters().length:0);
       writeCommandHeader(output, command);
       writeCommandParameters(output, command);
+      log.infof("ReplicableCommandExternalizer.writeObject() 2. finished writing new object");
    }
 
    protected static void writeCommandParameters(ObjectOutput output, ReplicableCommand command) throws IOException {
       Object[] args = command.getParameters();
       int numArgs = (args == null ? 0 : args.length);
-
+      log.infof("ReplicableCommandExternalizer.writeCommandParameters() for %d arguments",
+              numArgs);
       UnsignedNumeric.writeUnsignedInt(output, numArgs);
       for (int i = 0; i < numArgs; i++) {
          Object arg = args[i];
          if (arg instanceof DeltaAware) {
             // Only write deltas so that replication can be more efficient
             DeltaAware dw = (DeltaAware) arg;
-            output.writeObject(dw.delta());
+            Object theDelta = dw.delta();
+            output.writeObject(theDelta);
+            log.infof("ReplicableCommandExternalizer.writeCommandParameters() parameter #%d is DeltaAware, only wrote the Delta object = %s",
+                     i, theDelta);
+
          } else {
             output.writeObject(arg);
+            log.infof("ReplicableCommandExternalizer.writeCommandParameters() parameter #%d is normal object=%s",
+                     i, arg);
+
          }
       }
    }
@@ -92,32 +107,60 @@ public class ReplicableCommandExternalizer extends AbstractExternalizer<Replicab
       Collection<Class<? extends ReplicableCommand>> moduleCommands = getModuleCommands();
       // Write an indexer to separate commands defined external to the
       // infinispan core module from the ones defined via module commands
-      if (moduleCommands != null && moduleCommands.contains(command.getClass()))
+      if (moduleCommands != null && moduleCommands.contains(command.getClass())) {
          output.writeByte(1);
-      else
+        log.infof("ReplicableCommandExternalizer.writeCommandHeader() moduleCommands contains command (%s) class, wrote type=1",
+                command.getClass().getName());
+
+      }
+      else {
          output.writeByte(0);
+         log.infof("ReplicableCommandExternalizer.writeCommandHeader() moduleCommands (%s) DOES NOT contain command (%s) class, wrote type=0",
+                 moduleCommands, command.getClass().getName());
+      }
 
       output.writeShort(command.getCommandId());
+      log.infof("ReplicableCommandExternalizer.writeCommandHeader() wrote (short) commandId=%d",
+              command.getCommandId());
    }
 
    @Override
    public ReplicableCommand readObject(ObjectInput input) throws IOException, ClassNotFoundException {
+      log.infof("ReplicableCommandExternalizer.readObject() 1. started");
       byte type = input.readByte();
+      log.infof("ReplicableCommandExternalizer.readObject() 2. type=%d", type);
       short methodId = input.readShort();
+      log.infof("ReplicableCommandExternalizer.readObject() 3. (commandId) methodId=%d", methodId);
       Object[] args = readParameters(input);
-      return cmdFactory.fromStream((byte) methodId, args, type);
+      log.infof("ReplicableCommandExternalizer.readObject() 4. (commandId) arguments read for length=%d",
+              args != null? args.length:0);
+      ReplicableCommand command = cmdFactory.fromStream((byte) methodId, args, type);
+
+      log.infof("ReplicableCommandExternalizer.readObject() 2. read commandId=%d with parameter count=%d, command class=%s ",
+              command.getCommandId(),
+              command.getParameters() != null? command.getParameters().length:0,
+              command.getClass().getName());
+      return command;
    }
 
    protected Object[] readParameters(ObjectInput input) throws IOException, ClassNotFoundException {
+      log.infof("ReplicableCommandExternalizer.readParameters() entered 1.");
       int numArgs = UnsignedNumeric.readUnsignedInt(input);
+      log.infof("ReplicableCommandExternalizer.readParameters() 2. numArgs=%d", numArgs);
       Object[] args = null;
       if (numArgs > 0) {
+         log.infof("ReplicableCommandExternalizer.readParameters() 3. reading the parameters");
          args = new Object[numArgs];
          // For DeltaAware instances, nothing special to be done here.
          // Do not merge here since the cache contents are required.
          // Instead, merge in PutKeyValueCommand.perform
-         for (int i = 0; i < numArgs; i++) args[i] = input.readObject();
+         for (int i = 0; i < numArgs; i++) {
+             args[i] = input.readObject();
+             log.infof("ReplicableCommandExternalizer.readParameters() 4. read parameter#%d of object=%s",
+                     i, args[i]);
+         }
       }
+      log.infof("ReplicableCommandExternalizer.readParameters() done 5.");
       return args;
    }
 
